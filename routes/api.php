ebb0ffefd9f52d82a9fe1c8ce2918478e58bfd14
Mohamed Attar <mohammedattar0100020@gmail.com>
2023-03-14 12:26:19 +0200

    Update

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('test' , function(){
-    return 'Testing Endpoint';
+Route::post('test' , function(Request $request){
+    return $request->all();
 });
