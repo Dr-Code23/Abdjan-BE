562167730ec264d3a5b02ea9361045f736af71dc
Mohamed Attar <mohammedattar0100020@gmail.com>
2023-03-17 16:54:46 +0200

    Update

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('login' , [AuthController::class , 'login']);
 
-Route::group(['middleware' => ['auth:api']] , function(){
+Route::group(['middleware' => ['guest']] , function(){
 
     // Users
     Route::apiResource('users' , UserController::class);
