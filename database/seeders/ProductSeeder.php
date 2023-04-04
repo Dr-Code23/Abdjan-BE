6c6493923bf3bf2b92812dacfddd134dbd10171b
Mohamed Attar <mohammedattar0100020@gmail.com>
2023-04-04 20:14:28 +0200

    Update DataBaseSeeder.php

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Seeder;
 
 class ProductSeeder extends Seeder
 {
-    public static int $recordsCount = 100;
+    public static int $recordsCount = 50000;
 
     /**
      * Run the database seeds.
