13e4ec5b672cf958ef157bb9b43d7d288e6c095c
Mohamed Attar <mohammedattar0100020@gmail.com>
2023-04-09 01:39:06 +0200

    Update Dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -40,17 +40,20 @@ class DashboardController extends Controller
         );
 
         return $this->resourceResponse([
-            'productsCount' => $productsCount,
-            'servicesCount' => $servicesCount,
-            'allProjectsCount' => $allProjectsCount,
-            'pendingProjectsCount' => $pendingProjectsCount,
-            'doneProjectsCount' => $doneProjectsCount,
-            'dailyPayments' => round($dailyPayments , 2),
-            'dailyExpenses' => round($dailyExpenses , 2),
-            'monthlyPayments' => round($monthlyPayments , 2),
-            'monthlyExpenses' => round($monthlyExpenses , 2),
-            'yearlyPayments' => round($yearlyPayments , 2),
-            'yearlyExpenses' => round($yearlyExpenses , 2)
+            'productsCount' => number_format($productsCount),
+            'servicesCount' => number_format($servicesCount),
+            'allProjectsCount' => number_format($allProjectsCount),
+            'pendingProjectsCount' => number_format($pendingProjectsCount),
+            'doneProjectsCount' => number_format($doneProjectsCount),
+            'dailyPayments' => number_format(round($dailyPayments , 2)),
+            'dailyExpenses' => number_format(round($dailyExpenses , 2)),
+            'monthlyPayments' => number_format(round($monthlyPayments , 2)),
+            'monthlyExpenses' => number_format(round($monthlyExpenses , 2)),
+            'yearlyPayments' => number_format(round($yearlyPayments , 2)),
+            'yearlyExpenses' => number_format(round($yearlyExpenses , 2)),
+            'dailyProfits' => ($dailyPayments - $dailyExpenses) > 0 ? number_format(round(($dailyPayments - $dailyExpenses) , 2)) : '0',
+            'monthlyProfits' => ($monthlyPayments - $monthlyExpenses) > 0 ? number_format(round(($monthlyPayments - $monthlyExpenses) , 2)) : '0',
+            'yearlyProfits' => ($yearlyPayments - $yearlyExpenses) > 0 ? number_format(round(($yearlyPayments - $yearlyExpenses) , 2)) : '0',
         ]);
     }
 }
