fb4d0dea7311d460140565a40f5bb2464af5003f
Mohamed Attar <mohammedattar0100020@gmail.com>
2023-04-09 15:45:22 +0200

    Update Staging Script

--- install.staging.sh
+++ install.staging.sh
@@ -9,17 +9,17 @@ cp .env.staging .env
 
 # Generating App Key
 echo "Generating App Key"
-/usr/bin/php artisan key:gen --quiet
+/usr/bin/php artisan key:gen --quiet --force
 echo "Done"
 
 # Start Migrating Database And Seeding Dumpy Data
 echo "Migrating DB and Seeding Data"
-/usr/bin/php artisan migrate --seed --quiet
+/usr/bin/php artisan migrate:fresh --seed --quiet
 echo "Done"
 
 # Generating JWT Secret
 echo "Generating JWT Secret"
-sudo php artisan jwt:secret --quiet
+sudo php artisan jwt:secret --quiet --force
 echo "Done"
 
 # Shortcut For Storage Directory
